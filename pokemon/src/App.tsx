9472f3bae408bd49b2a09effdc0399d29d85a3e8
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-12-01 23:50:17 -0300

    create header and navbar

--- pokemon/src/App.tsx
+++ pokemon/src/App.tsx
@@ -1,7 +1,47 @@
+import * as S from './styled';
+import Filter from './assets/svg/filter';
+import Lupe from './assets/svg/lupe';
+import Star from './assets/svg/star';
+import Button from './components/button';
+import Text from './components/text';
 function App() {
 
   return (
     <div className="App">
+      <S.Header>
+        <S.PokemonLogo />
+        <S.WrapperPokeBall>
+          <S.Pokeball />
+        </S.WrapperPokeBall>
+      </S.Header>
+      <S.NavBar>
+        <S.WrapperButton>
+          <Button height='none' onClick={() => console.log('teste')}>
+            <S.WrapperChildrenButton>
+              <Filter />
+              <Text color='#fff' fontSize='15px'>
+                Filter
+              </Text>
+            </S.WrapperChildrenButton>
+          </Button>
+          <Button height='none' onClick={() => console.log('teste')}>
+            <S.WrapperChildrenButton>
+              <Star />
+              <Text color='#fff' fontSize='15px'>
+                Favorites
+              </Text>
+            </S.WrapperChildrenButton>
+          </Button>
+          <Button height='none' onClick={() => console.log('teste')}>
+            <S.WrapperChildrenButton>
+              <Lupe />
+              <Text color='#fff' fontSize='15px'>
+                Search
+              </Text>
+            </S.WrapperChildrenButton>
+          </Button>
+        </S.WrapperButton>
+      </S.NavBar>
     </div>
   )
 };
---
+++ pokemon/src/components/text/styled.ts
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+interface TextProps{
+    fontSize: string
+    bold: boolean
+    color: string
+    margin: string
+}
+
+export const Text = styled.p<TextProps>`
+    font-size: ${({fontSize}) => fontSize};
+    font-weight: ${({bold}) => bold ? '700' : 'normal'};
+    color: ${({color}) => color};
+    margin: ${({margin}) => margin};
+`;
