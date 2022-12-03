f9038c6e9e730c16141a3ca38ce35b7d6cbfabcc
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-12-03 17:48:23 -0300

    fix svg star colors

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 import * as S from './styled';
 import Filter from '../../assets/svg/filter';
 import Lupe from '../../assets/svg/lupe';
-import Star from '../../assets/svg/star';
+import Star from '../../assets/svg/Star';
 import theme from '../../styles/theme';
 import Text from '../text';
 import Button from '../button';
@@ -22,7 +22,7 @@ const NavBar: FC = () => {
           </Button>
           <Button height='none' onClick={() => console.log('teste')}>
             <S.WrapperChildrenButton>
-              <Star />
+              <Star size={31} stroke={theme.white} fill={theme.darkBlue}/>
               <Text color={theme.white} fontSize='15px'>
                 Favorites
               </Text>
