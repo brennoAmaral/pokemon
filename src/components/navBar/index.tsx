040d76a1afe9517ea06430c964551f6d72dce7b0
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-12-04 18:54:47 -0300

    implement funtion onFilter

--- src/components/navBar/index.tsx
+++ src/components/navBar/index.tsx
@@ -7,36 +7,41 @@ import theme from '../../styles/theme'
 import Text from '../text'
 import Button from '../button'
 
-const NavBar: FC = () => {
+interface NavBarProps {
+  onFilter: () => void
+}
+
+const NavBar: FC<NavBarProps> = (props) => {
+  const { onFilter } = props
   return (
     <S.NavBar>
-        <S.WrapperButton>
-          <Button height='none' onClick={() => console.log('teste')}>
-            <S.WrapperChildrenButton>
-              <Filter />
-              <Text color={theme.white} fontSize='15px'>
-                Filter
-              </Text>
-            </S.WrapperChildrenButton>
-          </Button>
-          <Button height='none' onClick={() => console.log('teste')}>
-            <S.WrapperChildrenButton>
-              <Star size={31} stroke={theme.white} fill={theme.darkBlue}/>
-              <Text color={theme.white} fontSize='15px'>
-                Favorites
-              </Text>
-            </S.WrapperChildrenButton>
-          </Button>
-          <Button height='none' onClick={() => console.log('teste')}>
-            <S.WrapperChildrenButton>
-              <Lupe />
-              <Text color={theme.white} fontSize='15px'>
-                Search
-              </Text>
-            </S.WrapperChildrenButton>
-          </Button>
-        </S.WrapperButton>
-      </S.NavBar>
+      <S.WrapperButton>
+        <Button height='none' onClick={() => console.log('teste')}>
+          <S.WrapperChildrenButton>
+            <Filter />
+            <Text color={theme.white} fontSize='15px'>
+              Filter
+            </Text>
+          </S.WrapperChildrenButton>
+        </Button>
+        <Button height='none' onClick={() => console.log('teste')}>
+          <S.WrapperChildrenButton>
+            <Star size={31} stroke={theme.white} fill={theme.darkBlue} />
+            <Text color={theme.white} fontSize='15px'>
+              Favorites
+            </Text>
+          </S.WrapperChildrenButton>
+        </Button>
+        <Button height='none' onClick={() => onFilter()}>
+          <S.WrapperChildrenButton>
+            <Lupe />
+            <Text color={theme.white} fontSize='15px'>
+              Search
+            </Text>
+          </S.WrapperChildrenButton>
+        </Button>
+      </S.WrapperButton>
+    </S.NavBar>
   )
 }
 
