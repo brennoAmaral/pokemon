e84a55a9bf64f5bb721652c366a91d5598c72381
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-12-04 10:55:29 -0300

    implement search bar

---
+++ src/components/searchBar/index.tsx
@@ -0,0 +1,31 @@
+import { FC, useCallback, useEffect, useRef, useState } from 'react'
+import Lupe from '../../assets/svg/lupe'
+import theme from '../../styles/theme'
+import * as S from './styled'
+import Button from '../button'
+
+const SearchBar: FC = (props) => {
+  const inputRef = useRef<string>('')
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [inputSearch, setInputSearch] = useState<string>('')
+  const handleSearch = useCallback(() => {
+    setInputSearch(inputRef.current.value)
+  }, [])
+  const handleSearchBar = useCallback((value: boolean): void => {
+    if (!value) {
+      setIsOpen(true)
+    } else {
+      setIsOpen(false)
+    }
+  }, [])
+  return (
+    <S.Wrapper isOpen={isOpen}>
+      <S.SearchBar placeholder='enter a pokemons name' ref={inputRef} />
+      <Button onClick={() => handleSearch()} width='auto' height='auto'>
+        <Lupe size='25px' fill={theme.darkBlue} stroke={theme.darkBlue} />
+      </Button>
+    </S.Wrapper>
+  )
+}
+
+export default SearchBar
---
+++ src/components/searchBar/styled.ts
@@ -0,0 +1,34 @@
+import styled from 'styled-components'
+
+interface WrapperProps {
+  isOpen: boolean
+}
+
+export const SearchBar = styled.input`
+  border: none;
+  font-size: 22px;
+  font-weight: 590;
+  color: ${({ theme }) => theme.darkBlue};
+  background-color: ${({ theme }) => theme.lightGray};
+  width: 85%;
+  ::placeholder{
+    font-size: 20px;
+    color: ${({ theme }) => theme.darkBlue};
+  }
+`
+
+export const Wrapper = styled.div<WrapperProps>`
+  border-radius: 50px;
+  width: 300px;
+  height: 40px;
+  padding: 0 20px;
+  border: 2px solid ${({ theme }) => theme.red};
+  background-color: ${({ theme }) => theme.lightGray};
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  filter: drop-shadow(5px 5px 10px #000);
+  transition: 1s;
+  position: absolute;
+  bottom: ${({isOpen}) => isOpen ? '200px' : '0px'}
+`
