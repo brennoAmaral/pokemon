5be485d89736c7af33b013a8397774fcd94600db
brennoAmaral <brenno.amaral.marques@gmail.com>
2022-12-04 08:50:34 -0300

    implement card adn modal render using context

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,65 @@
 import Card from './components/card'
-import { useCallback } from 'react'
+import { useCallback, useContext, useState } from 'react'
 import Header from './components/header'
 import NavBar from './components/navBar'
 import * as S from './style'
 import Modal from './components/modal'
+import { PokemonSetupContext } from './context/PokemonSetupProvider'
 
-const pokemonMock = [
-  {
-    id: 132,
-    name: 'ditto',
-    type: 'normal',
-    sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
-    fav: true
-  },
-  {
-    id: 25,
-    name: 'pikachu',
-    type: 'eletric',
-    sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
-    fav: false
-  }
-]
+// const pokemonMock = [
+//   {
+//     id: 132,
+//     name: 'ditto',
+//     type: 'normal',
+//     sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png',
+//     fav: true
+//   },
+//   {
+//     id: 25,
+//     name: 'pikachu',
+//     type: 'eletric',
+//     sprite: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/25.png',
+//     fav: false
+//   }
+// ]
 
 function App (): JSX.Element {
-  // const { pokeArray } = useContext(PokemonSetupContext)
-
+  const { pokemons, handleModal } = useContext(PokemonSetupContext)
   const renderPoke = useCallback((pokemons: any) => {
     return pokemons.map((pokemon: any) => {
       return (
-        <Card key={pokemon.sprite} name={pokemon.name} id={pokemon.id} sprite={pokemon.sprite} type={pokemon.type} fav={pokemon.fav} />
+        <Card
+          key={pokemon.sprite}
+          name={pokemon.name}
+          id={pokemon.id}
+          sprite={pokemon.sprite}
+          type={pokemon.type}
+          fav={pokemon.fav}
+          onClick={() => handleModal(pokemon.name, pokemons)}
+        />
+      )
+    })
+  }, [])
+
+  const renderModal = useCallback((pokemons: any) => {
+    return pokemons.map((pokemon: any) => {
+      console.log(pokemon.name, ': ', pokemon.modalIsOpen)
+      return (
+        <Modal
+          key={pokemon.sprite}
+          fav={pokemon.fav}
+          isOpen={pokemon.modalIsOpen}
+          name={pokemon.name}
+          sprite={pokemon.sprite}
+          id={pokemon.id}
+          type={pokemon.type}
+          hp={10}
+          attack={10}
+          especialAttack={10}
+          defense={10}
+          especialDefense={10}
+          weight={10}
+        />
       )
     })
   }, [])
@@ -36,10 +67,11 @@ function App (): JSX.Element {
   return (
     <div className="App">
       <>
-      <Modal isOpen={true} fav={false} name={pokemonMock[0].name} id={pokemonMock[0].id} sprite={pokemonMock[0].sprite} type={pokemonMock[0].type}/>
+        {/* <Modal isOpen={true} fav={false} name={pokemonMock[1].name} id={pokemonMock[1].id} sprite={pokemonMock[1].sprite} type={pokemonMock[1].type}/> */}
         <Header />
         <S.WrapperList>
-          {renderPoke(pokemonMock)}
+          {renderPoke(pokemons)}
+          {renderModal(pokemons)}
         </S.WrapperList>
         <NavBar />
       </>
